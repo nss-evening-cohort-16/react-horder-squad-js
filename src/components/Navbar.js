caadd98e7a62b4d2f2aac5e099d19ec4945272c3
Daniel Sitarek <deltasierra777@gmail.com>
2021-10-26 18:37:44 -0500

    created navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,28 +26,36 @@ const StuffNavbar = ({ user }) => {
         <NavbarBrand href="/">React Hoarder</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/New">New</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                <img className="user-img" src={user.profilePic} alt="user" />{user.user}
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  <NavLink className="sign-out-user" onClick={signOutUser}>Sign Out</NavLink>
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+          <Nav className="container-fluid" navbar>
+            {user ? (
+              <>
+                <NavItem>
+                  <NavLink href="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/New">New</NavLink>
+                </NavItem>
+                <UncontrolledDropdown className="user-menu" nav inNavbar>
+                  <DropdownToggle nav caret>
+                    <img className="user-img" src={user.profilePic} alt="user" />{user.user}
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      <NavLink className="sign-out-user" onClick={signOutUser}>Sign Out</NavLink>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </>
+            ) : ''}
           </Nav>
         </Collapse>
       </Navbar>
     </div>
   );
+};
+
+StuffNavbar.defaultProps = {
+  user: null,
 };
 
 StuffNavbar.propTypes = {
@@ -56,7 +64,7 @@ StuffNavbar.propTypes = {
     profilePic: PropTypes.string,
     uid: PropTypes.string,
     user: PropTypes.string,
-  }).isRequired,
+  }),
 };
 
 export default StuffNavbar;
