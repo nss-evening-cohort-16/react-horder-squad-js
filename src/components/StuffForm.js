b27da68f92eef0f38a5f6bed62e5da11a1e2cf16
DerekMalone <laxmalone@gmail.com>
2021-10-26 18:52:37 -0500

    saving to pull new main

---
+++ src/components/StuffForm.js
@@ -0,0 +1,17 @@
+// import React, { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+
+// const initialState = {
+
+// }
+
+// export default function StuffForm() {
+//   const [formInput, setFormInput] = useState({});
+//   const history = useHistory();
+
+//   return (
+//   <div>
+
+//   </div>
+//   );
+// }
