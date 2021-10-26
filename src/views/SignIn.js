26af3e309d631389f45f2fc2c393294795a762cc
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-10-25 21:50:16 -0500

    Sign In

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Spinner } from 'reactstrap';
 import { signInUser } from '../api/auth';
 
-export default function SignIn() {
+export default function SignIn({ user }) {
   return (
-    <div className="text-center mt-5">
-      <h1>Welcome! Sign In!</h1>
-      <button type="button" className="btn btn-success" onClick={signInUser}>
-        Sign In
-      </button>
-    </div>
+    <>
+      {user === null ? (
+        <div className="text-center">
+          <Spinner
+            style={{ width: '10rem', height: '10rem' }}
+            color="warning"
+          />
+        </div>
+      ) : (
+        <div className="text-center mt-5">
+          <h1>Welcome! Sign In!</h1>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={signInUser}
+          >
+            Sign In
+          </button>
+        </div>
+      )}
+    </>
   );
 }
+
+SignIn.propTypes = {
+  user: PropTypes.node.isRequired,
+};
