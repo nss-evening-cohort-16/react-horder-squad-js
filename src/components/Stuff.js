a09c608bf9d446aea6dff6817fdcfc24a5633947
Nathan Blackman <nathanpgblackman@gmail.com>
2021-10-26 19:52:20 -0500

    working on edit function

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -1,9 +1,14 @@
-import React from 'react';
+// import React from 'react';
 
-export default function Stuff() {
-  return (
-    <div>
-      <h1>Stuff!</h1>
-    </div>
-  );
-}
+// export default function Stuff({ stuff, setStuff, }) {
+//   const handleClick = (method) => {
+//     if (method === 'delete') {
+//       deleteStuff(stuff.firebaseKey).then(setStuff);
+//     }
+//   }
+//   return (
+//     <div>
+//       <h1>Stuff!</h1>
+//     </div>
+//   );
+// }
---
+++ src/data/stuffData.js
@@ -0,0 +1,6 @@
+// import axios from "axios";
+// import firebaseConfig from '../apiKeys';
+
+// const baseURL = firebaseConfig.databaseURL;
+
+// const updateStuff = () => new Promise((resolve, reject))
