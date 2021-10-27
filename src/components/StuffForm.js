f468995c53567067f8946c4286b24eee1cb17a0a
DerekMalone <laxmalone@gmail.com>
2021-10-26 19:12:22 -0500

    saving to pull new branch

--- src/components/StuffForm.js
+++ src/components/StuffForm.js
@@ -1,17 +1,46 @@
-// import React, { useState } from 'react';
+// import { objectOf } from 'prop-types';
+// import React, { useState, useEffect } from 'react';
 // import { useHistory } from 'react-router-dom';
 
 // const initialState = {
+//   itemDescription: '',
+//   itemImage: '',
+//   itemName: '',
+//   uid: '',
+// };
 
-// }
-
-// export default function StuffForm() {
+// export default function StuffForm({ obj }) {
 //   const [formInput, setFormInput] = useState({});
 //   const history = useHistory();
 
-//   return (
-//   <div>
+//   useEffect(() => {
+//     if (obj.firebaseKey) {
+//       setFormInput({
+//         itemDescription: obj.itemDescription,
+//         itemImage: obj.itemImage,
+//         itemName: obj.itemName,
+//         uid: obj.uid,
+//       });
+//     } else {
+//       setFormInput(initialState);
+//     }
+//   }, [obj]);
 
-//   </div>
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormInput((preState) => ({
+//       ...preState,
+//       [name]: value,
+//     }));
+//   };
+
+//   //   const handleSubmit = () => {
+
+//   //   };
+
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//     </div>
 //   );
 // }
