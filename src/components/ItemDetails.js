729dec2ab5c1ddbf3207f1fe5bfa2662d1e45cba
Daniel Sitarek <deltasierra777@gmail.com>
2021-10-26 21:21:24 -0500

    added delete to item detail

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getSingleItem } from '../data/stuffData';
+import { useParams, Link, useHistory } from 'react-router-dom';
+import { getSingleItem, deleteItem } from '../data/stuffData';
 
 export default function ItemDetails() {
   const { fbKey } = useParams({});
   const [item, setItem] = useState({});
+
+  const history = useHistory();
+
+  const handleDelete = () => {
+    deleteItem(item.firebaseKey).then(() => history.push('/'));
+  };
 
   useEffect(() => {
     getSingleItem(fbKey).then((stuff) => setItem(stuff));
@@ -15,6 +21,15 @@ export default function ItemDetails() {
       <img className="item-details-img" src={item.itemImage} alt={item.itemName} />
       <h1>{item.itemName}</h1>
       <p>{item.itemDescription}</p>
+      <Link to={`/edit/${item.firebaseKey}`} className="btn btn-warning">
+        Edit
+      </Link>
+      <Link to={`/details/${item.firebaseKey}`} className="btn btn-info">
+        Details
+      </Link>
+      <button type="button" className="btn btn-danger" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 }
