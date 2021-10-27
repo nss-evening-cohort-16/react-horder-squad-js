f4ab4ce8b9398b28ee90e3e75122753ea5a45405
Daniel Sitarek <deltasierra777@gmail.com>
2021-10-26 20:22:38 -0500

    added delete feature to cards

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { getSingleItem } from '../data/stuffData';
 
 export default function ItemDetails() {
@@ -15,14 +15,6 @@ export default function ItemDetails() {
       <img className="item-details-img" src={item.itemImage} alt={item.itemName} />
       <h1>{item.itemName}</h1>
       <p>{item.itemDescription}</p>
-      <div className="item-details-btns">
-        <Link to={`/edit/${item.firebaseKey}`} className="btn btn-warning">
-          Edit
-        </Link>
-        <button type="button" className="btn btn-danger">
-          Delete
-        </button>
-      </div>
     </div>
   );
 }
