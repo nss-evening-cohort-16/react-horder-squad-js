10d38840bede4d7a4aaa2cda6bf448342f7ee403
Nathan Blackman <nathanpgblackman@gmail.com>
2021-10-28 18:46:21 -0500

    getting the app to start

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,9 +1,21 @@
-import React from 'react';
+// import React from 'react';
+// import { useHistory } from 'react-router-dom';
+// import { getSingleItem } from '../data/stuffData';
 
-export default function Edit() {
-  return (
-    <div>
-      <h1>Edit!</h1>
-    </div>
-  );
-}
+// export default function Edit({ user }) {
+//   const [formInput, setFormInput] = useState({});
+//   const { fbKey } = useParams();
+//   const history = useHistory();
+
+//   useEffect(() => {
+//     getSingleItem().then(() => {
+
+//     })
+//   })
+
+//   return (
+//     <div>
+//       <h1>Edit!</h1>
+//     </div>
+//   );
+// }
